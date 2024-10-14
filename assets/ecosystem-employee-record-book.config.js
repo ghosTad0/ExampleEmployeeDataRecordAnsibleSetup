6e4d318691c0b0b73ff4e8b5356ac1a4839d098d
tad <tuan.a@hsenidmobile.com>
2024-10-15 02:41:53 +0530

    changes to match the production build deploy

--- assets/ecosystem-employee-record-book.config.js
+++ assets/ecosystem-employee-record-book.config.js
@@ -2,9 +2,9 @@ module.exports = {
     apps: [
       {
         name: "employee-record-book",
-        script: "npm",
-        args: "run dev",
-        cwd: "/hms/apps/ExampleEmployeeDataRecord",  //refer the app name from ansible varibles.
+        script: "node",
+        args: "server.js",
+        cwd: "/hms/apps/ExampleEmployeeDataRecord/__production_build",  //refer the app name from ansible varibles.
         log_file: "/hms/logs/employee_record_book_logs/employee_record_book.log",
         out_file: "/hms/logs/employee_record_book_logs/employee_record_book-out.log",
         error_file: "/hms/logs/employee_record_book_logs/employee_record_book-error.log",
